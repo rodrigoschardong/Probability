b0152126a2524143a05c5b20fed3a5923d1ef251
rodrigoschardong <rscharong@gmail.com>
2022-04-02 10:55:23 -0300

    Adding Jupyter Notebook and Fixing tests

--- 1-Probabilities/A_coin_probability.py
+++ 1-Probabilities/A_coin_probability.py
@@ -8,6 +8,7 @@ import numpy as np
 import math
 import unittest
 
+TEST = 0
 
 class Probability_F():
     """
@@ -29,12 +30,16 @@ class Probability_F():
                 trueAnswer += 1
         return trueAnswer / len(results)
 
-    def Experiment(self, probability, chances):
+    def Experiment(self, probability, chances, TEST = 0):
         if(chances != math.trunc(chances)):
             chances = math.trunc(chances)
         results  = []
         for i in range (chances):
-            results.append(self.Toss_A_Coin(Probability_F, probability))
+            if(TEST == 1):
+                results.append(self.Toss_A_Coin(Probability_F, probability))
+            else:
+                results.append(self.Toss_A_Coin(probability))
+
         return results
         
 
@@ -53,17 +58,17 @@ class Tests(unittest.TestCase):
         self.assertEqual(Probability_F.Frequency(Probability_F, [1,1,1,1,1,1]),  1, "Should be 1")
 
     def test_Experiment_zero(self):
-        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 0)),  0, "Should be 0")
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 0, 1)),  0, "Should be 0")
     def test_Experiment_five(self):
-        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 5)),  5, "Should be 5")
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 5, 1)),  5, "Should be 5")
     def test_Experiment_hundred(self):
-        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 100)),  100, "Should be 100")
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 100, 1)),  100, "Should be 100")
     def test_Experiment_negative(self):
-        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, -1)),  0, "Should be 0")
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, -1, 1)),  0, "Should be 0")
     def test_Experiment_broken_num(self):
-        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 2.5)),  2, "Should be 2")
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 2.5, 1)),  2, "Should be 2")
     def test_Experiment_negative_broken_num(self):
-        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, -2.5)),  0, "Should be 0")
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, -2.5, 1)),  0, "Should be 0")
     
 
 if __name__ == '__main__':

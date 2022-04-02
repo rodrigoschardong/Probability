cb8a6ee28b45e176dec69631a2ba063363c7115d
rodrigoschardong <rscharong@gmail.com>
2022-04-02 10:13:57 -0300

    Adding some tests

--- 1-Probabilities/1_coin_probability.py
+++ 1-Probabilities/1_coin_probability.py
@@ -5,10 +5,11 @@
 
 from unittest import result
 import numpy as np 
+import math
 import unittest
 
 
-class Probability():
+class Probability_F():
     """
     @brief  It's like toss a coin
     @note   Return true or false
@@ -23,24 +24,47 @@ class Probability():
 
     def Frequency(self, results):
         trueAnswer = 0
-        for i in range(results.shape[0]):
+        for i in range(len(results)):
             if(results[i] == 1):
                 trueAnswer += 1
-        return trueAnswer / results.shape[0]
+        return trueAnswer / len(results)
 
     def Experiment(self, probability, chances):
-        results = np.zeros((chances))
+        if(chances != math.trunc(chances)):
+            chances = math.trunc(chances)
+        results  = []
         for i in range (chances):
-            results[i] = self.Toss_A_Coin(probability)
-        return 
+            results.append(self.Toss_A_Coin(Probability_F, probability))
+        return results
         
 
 class Tests(unittest.TestCase):
+
     def test_Toss_A_Coin_zero_percent(self):
-        self.assertEqual(Probability.Toss_A_Coin(Probability, 0),  0, "Should be 0")
-        
+        self.assertEqual(Probability_F.Toss_A_Coin(Probability_F, 0),  0, "Should be 0")       
     def test_Toss_A_Coin_hundred_percent(self):
-        self.assertEqual(Probability.Toss_A_Coin(Probability, 1),  1, "Should be 1")
+        self.assertEqual(Probability_F.Toss_A_Coin(Probability_F, 1),  1, "Should be 1")
+
+    def test_Frequency_zero(self):
+        self.assertEqual(Probability_F.Frequency(Probability_F, [0,0,0,0,0,0]),  0, "Should be 0")
+    def test_Frequency_half(self):
+        self.assertEqual(Probability_F.Frequency(Probability_F, [0,1,0,1,0,1]),  0.5, "Should be 0.5")
+    def test_Frequency_one(self):
+        self.assertEqual(Probability_F.Frequency(Probability_F, [1,1,1,1,1,1]),  1, "Should be 1")
+
+    def test_Experiment_zero(self):
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 0)),  0, "Should be 0")
+    def test_Experiment_five(self):
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 5)),  5, "Should be 5")
+    def test_Experiment_hundred(self):
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 100)),  100, "Should be 100")
+    def test_Experiment_negative(self):
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, -1)),  0, "Should be 0")
+    def test_Experiment_broken_num(self):
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, 2.5)),  2, "Should be 2")
+    def test_Experiment_negative_broken_num(self):
+        self.assertEqual(len(Probability_F.Experiment(Probability_F, 0, -2.5)),  0, "Should be 0")
+    
 
 if __name__ == '__main__':
     unittest.main()
